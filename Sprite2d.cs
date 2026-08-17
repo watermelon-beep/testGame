ebb4ba27dcdf422de3a94ae51758e3380d35d7b4
WATERMELON <WATERMELON@DESKTOP-4UPNDSR>
2026-08-17 20:22:55 +0800

    initial

---
+++ Sprite2d.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class Sprite2d : Sprite2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		float amount = 5;
+		if(Input.IsKeyPressed(Key.W)) {
+			this.Position += new Vector2(0, -amount);
+		}
+		if(Input.IsKeyPressed(Key.D)) {
+			this.Position += new Vector2(amount, 0);
+		}
+		if(Input.IsKeyPressed(Key.S)) {
+			this.Position += new Vector2(0, amount);
+		}if(Input.IsKeyPressed(Key.A)) {
+			this.Position += new Vector2(-amount, 0);
+		}
+	}
+}
